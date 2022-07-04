b6388b4411e5cd9bbf1cf066cbb6d241eaf389c5
unknown <obirikan020@gmail.com>
2022-07-04 15:25:32 +0000

    ++footer

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import {Route} from 'react-router-dom'
 import Hero from './Components/Hero';
+import Footer from './Components/Footer';
 function App() {
   return (
     <div  >
-      <Route>
         <Navbar/>
-        <Hero/>
-      </Route>
+        <Route to='/' component={Hero} exact/>
+        <Footer/>
     </div>
   );
 }
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='flex justify-center items-center h-16 bg-black text-white'>
+        <p>OBERRY_GH</p>
+    </div>
+  )
+}
+
+export default Footer
